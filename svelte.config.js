3c9d7cee88747ae7f87af76ab8917a9c870f9d3e
Willian Amaral <willian.amaral@outlook.com.br>
2021-06-08 18:11:33 -0300

    Add build config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: '#svelte'
 	}
 };
